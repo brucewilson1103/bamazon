3d8ccc6ee4548a22ce7ff9dbc6df13727bdb4af4
da Vinci <brucewilson1103@gmail.com>
2019-01-29 20:00:30 -0500

    initial work with files this prompts the user initially to post an item need to make an auction table in sql database

---
+++ bamazoncustomer.js
@@ -0,0 +1,100 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+require('dotenv').config()
+
+var connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: process.env.DB_USER,
+
+  // Your password
+  password: process.env.DB_PASS,
+  database: "bamazon"
+});
+
+
+connection.connect(function(err) {
+  if (err) throw err;
+  // run the start function after the connection is made to prompt the user
+  // start();
+  console.log("connected");
+  console.log("_________________________________________")
+  connection.query("SELECT * FROM products", function (err, results) {
+    if (err) throw err;
+    
+    for (var i=0; i<results.length; i++){
+    console.log(results[i].product_name)}
+    start();
+  });
+});
+function start() {
+  inquirer
+    .prompt({
+      name: "postOrBid",
+      type: "list",
+      message: "Would you like to [POST] an auction or [BID] on an auction?",
+      choices: ["POST", "BID", "EXIT"]
+    })
+    .then(function(answer) {
+      // based on their answer, either call the bid or the post functions
+      if (answer.postOrBid === "POST") {
+        postAuction();
+      }
+      else if(answer.postOrBid === "BID") {
+        bidAuction();
+      } else{
+        connection.end();
+      }
+    });
+}
+
+// function to handle posting new items up for auction
+function postAuction() {
+  // prompt for info about the item being put up for auction
+  inquirer
+    .prompt([
+      {
+        name: "item",
+        type: "input",
+        message: "What is the item you would like to submit?"
+      },
+      {
+        name: "category",
+        type: "input",
+        message: "What category would you like to place your auction in?"
+      },
+      {
+        name: "startingBid",
+        type: "input",
+        message: "What would you like your starting bid to be?",
+        validate: function(value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        }
+      }
+    ])
+    .then(function(answer) {
+      // when finished prompting, insert a new item into the db with that info
+      connection.query(
+        "INSERT INTO auctions SET ?",
+        {
+          item_name: answer.item,
+          category: answer.category,
+          starting_bid: answer.startingBid || 0,
+          highest_bid: answer.startingBid || 0
+        },
+        function(err) {
+          if (err) throw err;
+          console.log("Your auction was created successfully!");
+          // re-prompt the user for if they want to bid or post
+          start();
+        }
+      );
+    });
+}
